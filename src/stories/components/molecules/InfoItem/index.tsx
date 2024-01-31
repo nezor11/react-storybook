c6112a8e54c4c0cb9b3934425c2235bc338cbe6c
Jorge Mtnez <jmi@novicell.es>
2024-01-31 17:37:12 +0100

    add more coditions

--- src/stories/components/molecules/InfoItem/index.tsx
+++ src/stories/components/molecules/InfoItem/index.tsx
@@ -19,7 +19,7 @@ export const InfoItem = forwardRef<HTMLDivElement, InfoItemProps>(
     return (
       <div ref={ref} className={cn("grid grid-cols-2 gap-4")} {...props}>
         <div>
-          {info.company ? (
+          {info.company && (
             <>
               <Text
                 as="h5"
@@ -31,19 +31,28 @@ export const InfoItem = forwardRef<HTMLDivElement, InfoItemProps>(
                 {info.company}
               </Text>
             </>
-          ) : (
-            <></>
           )}
-
-          <Text as="h6" size={"sm"}>
-            {info.date}
-          </Text>
+          {info.date && (
+            <>
+              <Text as="h6" size={"sm"}>
+                {info.date}
+              </Text>
+            </>
+          )}
         </div>
         <div>
-          <Text as="h4" weight={"bold"} size={"xl"} className="mb-2">
-            {info.jobTitle}
-          </Text>
-          <Text as="p">{info.jobDesc}</Text>
+          {info.jobTitle && (
+            <>
+              <Text as="h4" weight={"bold"} size={"xl"} className="mb-2">
+                {info.jobTitle}
+              </Text>
+            </>
+          )}
+          {info.jobDesc && (
+            <>
+              <Text as="p">{info.jobDesc}</Text>
+            </>
+          )}
         </div>
       </div>
     );
