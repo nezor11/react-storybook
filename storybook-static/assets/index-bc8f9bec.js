c207788a3434292d205d67b45e8830d4851860ea
Jorge Mtnez <jmi@novicell.es>
2024-05-08 16:12:14 +0200

    update package

--- storybook-static/assets/index-bc8f9bec.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./jsx-runtime-9c4ae004.js";import{I as e}from"./index-aa594a3c.js";import{c}from"./index-a4eac33a.js";import{r as i}from"./index-1b03fe98.js";const j=i.forwardRef(({resumeItems:o,...s},t)=>r.jsx("article",{ref:t,className:c("my-10"),...s,children:o&&r.jsx(r.Fragment,{children:o.map((a,m)=>r.jsx(e,{...a},m))})}));export{j as R};
